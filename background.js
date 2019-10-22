46101de54e7185d8a67003a9e3e8ebdad7bba0c6
FreeWall <info@michalvanek.net>
2019-10-22 23:56:21 +0200

    Proxy, improvements

--- background.js
+++ background.js
@@ -1,28 +1,10 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-});
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.channel == "deployments") {
-            var url = "https://pz-deployment.deployhq.com/projects/postovnezdarma-cz/deployments?to=3ae3682c-3491-0bc7-85c1-ccaab0101b9f";
-            fetch(url)
-                .then(response => response.text())
-                .then(response => sendResponse(response));
-            /*fetch(url, {
-				headers: {
-					"Accept": "application/json",
-					'Authorization': 'Basic '+btoa('user@domain.com:api-key'),
-				}
-            })*/
+            var url = "https://www.michalvanek.net/ateli/deployhq.php";
+            fetch(url).then(response => response.json()).then(response => sendResponse(response));
             return true;
         }
     }
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ $(function(){
 	chrome.runtime.sendMessage(
 		{channel: "deployments"},
 		function(response){
-			let deployments = JSON.parse($("div[data-react-class=\"DeployHQ.components.Deployments\"]",response).attr("data-react-props"))['deployments'];
+			let deployments = response['records'];
 			for(let i in deployments){
 				var row = $("div.Box-row").first().clone();
 				deploymentToRow(deployments[i],row);
@@ -14,21 +14,24 @@ $(function(){
 		}
 	);
 
+
 	function deploymentToRow(deployment,row){
 		$("label",row).remove();
-		$("a[data-hovercard-type=\"pull_request\"]",row).html("Manual deploy of "+deployment['project']['name']);
-		$("span[aria-label=\"Merged pull request\"]",row).html("<img src='"+chrome.extension.getURL('deployhq.png')+"' style='width:32px;vertical-align:middle;margin-right:7px;margin-left:2px;margin-top:5px;'/>");
+		$(".d-inline-block.mr-1",row).remove();
+		$(".float-right.col-3",row).remove();
+		$("a[data-hovercard-type=\"pull_request\"]",row).replaceWith("<div class='h4'>Manual deploy of "+deployment['project']['name']+"</div>");
+		$("span[aria-label=\"Merged pull request\"]",row).attr("aria-label","DeployHQ").html("<img src='"+chrome.extension.getURL('deployhq.png')+"' style='width:32px;vertical-align:middle;margin-right:7px;margin-left:2px;margin-top:5px;'/>");
 		let $date = $("span.issue-meta-section.ml-2",row).first().clone();
 		$date.html($date.html().replace('updated','deployed'));
-		let date = new Date(deployment['finishedAt']);
-		$("relative-time",$date).attr("datetime", deployment['finishedAt']).attr("title", date.toLocaleString());
-		$("div.mt-1.text-small",row).html("Deployed by <a data-hovercard-type='user' href='javascript:void(0);'>"+deployment['user']['name']+"</a> • ");
+		let date = new Date(deployment['timestamps']['completed_at']);
+		$("relative-time",$date).attr("datetime", deployment['timestamps']['completed_at']).attr("title", date.toLocaleString());
+		$("div.mt-1.text-small",row).html("Deployed by <a class='muted-link' data-hovercard-type='user' href='javascript:void(0);'>"+deployment['deployer']+"</a> • ");
 		if(deployment['status'] == "completed"){
-			$("div.mt-1.text-small",row).append("<a class='tooltipped' aria-label='approval' href='javascript:void(0);'>Completed</a>");
+			$("div.mt-1.text-small",row).append("<a class='tooltipped muted-link' aria-label='approval' href='javascript:void(0);'>Completed</a>");
 		} else if(deployment['status'] == "failed"){
-			$("div.mt-1.text-small",row).append("<a class='tooltipped' aria-label='requesting' href='javascript:void(0);'>Failed</a>");
+			$("div.mt-1.text-small",row).append("<a class='tooltipped muted-link' aria-label='requesting' href='javascript:void(0);'>Failed</a>");
 		} else {
-			$("div.mt-1.text-small",row).append("<a class='tooltipped' aria-label='required' href='javascript:void(0);'>"+deployment['status']+"</a>");
+			$("div.mt-1.text-small",row).append("<a class='tooltipped muted-link' aria-label='required' href='javascript:void(0);'>"+deployment['status']+"</a>");
 		}
 		$("div.mt-1.text-small",row).append($date);
 		row.css("background","#F4F4F4");
